b1c6cae81dbeb30cc05ce05a640b76388e1bf17b
Nohcnadib <nohcnadib@gmail.com>
2025-11-09 14:57:03 -0300

    Desactivar chequeo de tipos en Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Checker from 'vite-plugin-checker';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Checker({ typescript: false })
+  ],
 })
